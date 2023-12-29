58ac75e81237e9e09751fc0fa1413afe6b85679e
tatsu <78641196+tatsupro@users.noreply.github.com>
2023-12-29 14:40:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 DOTFILES_REPO="${1:-"https://github.com/tatsupro/dotfiles.git"}"
 DOTFILES_DIR="$HOME/.dotfiles"
 
-git init --bare $DOTFILES_DIR --quiet
+git init --bare $DOTFILES_DIR
 alias dot="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 exec $SHELL
 dot config --local status.showUntrackedFiles no
