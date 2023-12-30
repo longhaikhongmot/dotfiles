98dbc30485477b3b4d4f92105d6475076367f6a2
tatsu <78641196+tatsupro@users.noreply.github.com>
2023-12-30 10:26:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,14 +4,13 @@ DOTFILES_REPO="${1:-"https://github.com/tatsupro/dotfiles.git"}"
 DOTFILES_DIR="$HOME/.dotfiles"
 
 git init --bare $DOTFILES_DIR
-alias dot="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
-exec $SHELL
-dot config --local status.showUntrackedFiles no
+DOT="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
+$DOT config --local status.showUntrackedFiles no
 cat > "$DOTFILES_DIR/info/sparse-checkout" << EOM
 !README.md
 !LICENSE
 !setup.sh
 EOM
 
-dot branch -M main
-dot pull origin main
+$DOT branch -M main
+$DOT pull origin main
