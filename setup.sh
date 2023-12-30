0a89be25d758cf494ed9281bbdc6d68b4b62912b
tatsu <78641196+tatsupro@users.noreply.github.com>
2023-12-30 04:50:56 +0000

    typo

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ git init --bare $DOTFILES_DIR
 DOT="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
 $DOT config --local status.showUntrackedFiles no
 cat > "$DOTFILES_DIR/info/sparse-checkout" << EOM
+/*
 !README.md
 !LICENSE
 !setup.sh
