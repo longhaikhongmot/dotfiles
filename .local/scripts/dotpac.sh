b609812935205de49c29b17125b4ff39a91b2e28
tatsu <78641196+tatsupro@users.noreply.github.com>
2024-04-12 03:55:28 +0000

    Update dotpac.sh

--- .local/scripts/dotpac.sh
+++ .local/scripts/dotpac.sh
@@ -16,8 +16,15 @@ add command usage:
 }
 
 dotpac() {
-    pacman_list="https://raw.githubusercontent.com/tatsupro/dotfiles/main/.local/packages/pacman.csv"
-    homebrew_list="https://raw.githubusercontent.com/tatsupro/dotfiles/main/.local/packages/homebrew.csv"
+    local -r pacman_list="https://raw.githubusercontent.com/tatsupro/dotfiles/main/.local/packages/pacman.csv"
+    local -r homebrew_list="https://raw.githubusercontent.com/tatsupro/dotfiles/main/.local/packages/homebrew.csv"
+
+    local -r powerlevel10k_remote="https://github.com/romkatv/powerlevel10k.git"
+    local -r fast_syntax_highlighting_remote="https://github.com/zdharma-continuum/fast-syntax-highlighting.git"
+    local -r autosuggestions_remote="https://github.com/zsh-users/zsh-autosuggestions"
+    local -r powerlevel10k_path="$HOME/.local/share/powerlevel10k"
+    local -r fast_syntax_highlighting_path="$HOME/.local/share/zsh/plugins/fast-syntax-highlighting"
+    local -r autosuggestions_path="$HOME/.local/share/zsh/plugins/zsh-autosuggestions"
 
     if [ -z "$1" ]; then
         dotpac_manual
@@ -32,14 +39,19 @@ dotpac() {
     if [[ "$1" == "install" ]]; then
         if type pacman >/dev/null; then
             curl -s $pacman_list | tail -n +2 | cut -d ',' -f1 | xargs sudo pacman -Syu --noconfirm
-            return 0
         fi
 
         if type brew >/dev/null; then
             brew update && brew upgrade
             curl -s $homebrew_list | tail -n +2 | cut -d ',' -f1 | xargs brew install
-            return 0
         fi
+
+        rm -fr $powerlevel10k_path $fast-syntax-highlighting_path $autosuggestions_path
+        git clone --depth=1 $powerlevel10k_remote $powerlevel10k_path
+        git clone --depth=1 $fast_syntax_highlighting_remote $fast_syntax_highlighting_path
+        git clone --depth=1 $autosuggestions_remote $autosuggestions_path
+
+        return 0
     fi
 
     if [[ "$1" == "add" ]]; then
