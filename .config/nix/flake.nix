dcf543bed055cf1585ad322c5553e55c3b6e5540
Tatsu <78641196+tatsupro@users.noreply.github.com>
2024-10-08 05:21:29 +0700

    add nix

---
+++ .config/nix/flake.nix
@@ -0,0 +1,91 @@
+{
+  description = "Long does Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  let
+    configuration = { pkgs, ... }: {
+      # Install unfree packages
+      nixpkgs.config.allowUnfree = true;
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        neovim
+        pfetch-rs
+        htop
+        tmux
+        fzf
+        lf
+        terraform
+        kubectl
+        kubectx
+        kubernetes-helm
+        argocd
+        go
+        nodejs
+        tldr
+        jq
+        yq
+      ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "watch"
+          "iproute2mac"
+        ];
+        onActivation.cleanup = "zap";
+        onActivation.upgrade = true;
+        onActivation.autoUpdate = true;
+      };
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#pro
+    darwinConfigurations."pro" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            user = "longnguyen23";
+            enable = true;
+            autoMigrate = true;
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."pro".pkgs;
+  };
+}
