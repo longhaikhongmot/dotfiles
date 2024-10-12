402dc10458bdbe0789099ed5a063ed60cd6bb738
Long <long.nguyen23@onemount.com>
2024-10-13 03:16:09 +0700

    implement nix

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -56,6 +56,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.extraSpecialArgs = specialArgs;
           home-manager.users.${username} = import ./modules/home-manager;
         }
