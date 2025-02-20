c96dfe42929d3c6ec6d71e69f6def08ef67d4b86
Long <78641196+longwindingroad@users.noreply.github.com>
2025-02-20 11:52:05 +0700

    kubectx revert

--- .config/zsh/.p10k.zsh
+++ .config/zsh/.p10k.zsh
@@ -1328,11 +1328,12 @@
       '*prod*'  PROD    # These values are examples that are unlikely
       # '*prod*'  TEST    # to match your needs. Customize them as needed.
       '*'       DEFAULT)
-  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_FOREGROUND=2
   typeset -g POWERLEVEL9K_KUBECONTEXT_PROD_FOREGROUND=1
   typeset -g POWERLEVEL9K_KUBECONTEXT_PROD_VISUAL_IDENTIFIER_EXPANSION='🚨'
-  typeset -g POWERLEVEL9K_KUBECONTEXT_PROD_CONTENT_EXPANSION='> ${P9K_CONTENT} <'
-  # typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_PROD_CONTENT_EXPANSION='${P9K_CONTENT}'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_FOREGROUND=2
+  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_CONTENT_EXPANSION='${P9K_CONTENT}'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_DEFAULT_VISUAL_IDENTIFIER_EXPANSION='🪴'
 
   # Use POWERLEVEL9K_KUBECONTEXT_CONTENT_EXPANSION to specify the content displayed by kubecontext
   # segment. Parameter expansions are very flexible and fast, too. See reference:
